d10318503348ff026439d4b8fb1580897ab14bdb
Zhark10 <verygoodman18@yandex.ru>
2020-06-17 14:44:34 +0300

    js optimazation for tail call (tail recursion)

---
+++ recursion/tail_call_optimization.js
@@ -0,0 +1,34 @@
+function factorial_1(num, acc = 1) {
+    return (num === 1) ? acc : () => factorial_1(num - 1, acc * num)
+}
+
+function sum_1(num, acc = 0) {
+    return (num === 0) ? acc : () => sum_1(num - 1, acc + num)
+}
+
+// call-stack optimization
+
+function trampoline(fn) {
+    return function(...args) {
+      let result = fn.apply(fn.context, args)
+      while (typeof result === 'function') {
+        result = result()
+      }
+  
+      return result
+    }
+  }
+
+const trampolineFactorial = trampoline(factorial_1)
+const trampolineSum = trampoline(sum_1)
+
+const results_1 = {
+    'optimazed tail-call factorial': trampolineFactorial(100),
+    'optimazed tail-call sum': trampolineSum(999999),
+}
+
+var style_1 = ['padding: 1rem;',
+             'font: 1.3rem/3 Georgia;',
+             'color: green;'].join('');
+console.log('%c%s', style_1, 'optimized example')
+console.table(results_1)
