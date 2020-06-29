b75fb25f4b9839703ecda40608c41caa1ab1c019
Zhark10 <verygoodman18@yandex.ru>
2020-06-30 00:04:03 +0300

    advanced throttling (last call not ignored)

---
+++ throttling/advanced-throttle.js
@@ -0,0 +1,22 @@
+const advancedThrottle = (func, limit) => {
+  let lastFunc
+  let lastRan
+  return function() {
+    const context = this
+    const args = arguments
+    if (!lastRan) {
+      func.apply(context, args)
+      lastRan = Date.now()
+    } else {
+      clearTimeout(lastFunc)
+      lastFunc = setTimeout(function() {
+        if ((Date.now() - lastRan) >= limit) {
+          func.apply(context, args)
+          lastRan = Date.now()
+        }
+      }, limit - (Date.now() - lastRan))
+    }
+  }
+}
+
+// ToDo: data fetching, scrolling (example)
