84c489165fb5927382a8cee32a419c0741c8d531
Zhark10 <verygoodman18@yandex.ru>
2022-05-21 00:44:21 +0300

    array proxy refactoring (for optimal search)

--- arr-proxy/indexed.js
+++ arr-proxy/indexed.js
@@ -1,16 +1,32 @@
-const IndexedArray = new Proxy(Array, {
+const OptimizedArray = new Proxy(Array, {
   construct(target, [array]) {
-    const cachedArray = {ids: {}, names: {}}
-    array.forEach(item => cachedArray.ids[item.id] = cachedArray.names[item.name] = item)
+    const arrayFields = Object.keys(array[0])
+    const arrayToSearchByUID = {}
+    const cachedArray = {}
+    arrayFields.forEach(fieldName => cachedArray[fieldName] = {})
+
+    const updArrays = item => {
+         arrayFields.forEach(fieldName => {
+            const generatedUID = `UID-${Object.values(item).join("")}`
+            cachedArray[fieldName][item[fieldName]] = generatedUID
+            arrayToSearchByUID[generatedUID] = item
+        })
+    }
+      
+    array.forEach(updArrays)
+    
     return new Proxy(new target(...array), {
         get(targetArray, prop) {
             const proxyMethods = {
                 "push": item => {
-                    cachedArray.ids[item.id] = cachedArray.names[item.name] = item
+                    updArrays(item)
                     targetArray[prop].call(targetArray. prop)
                 },
-                "findByName": name => cachedArray.names[name],
-                "findById": id => cachedArray.names[id],
+                "findByField": (fieldName, searchValue) => {
+                    const foundUID = cachedArray[fieldName][searchValue]
+                    console.log('foundUID', foundUID)
+                    return arrayToSearchByUID[foundUID]
+                },
             }
             return proxyMethods[prop] || targetArray
         }
@@ -19,11 +35,15 @@ const IndexedArray = new Proxy(Array, {
 })
 
 const users = [
-  {id: 153, name: "Arkady", age: 27},
   {id: 163, name: "Max", age: 25},
   {id: 142, name: "Dasha", age: 32},
+  {id: 146, name: "Dmitry", age: 31},
+  {id: 153, name: "Arkady", age: 27},
 ]
 
-const indexedUsers = new IndexedArray(users)
+const indexedUsers = new OptimizedArray(users)
 
-console.log(indexedUsers.findByName("Arkady"))
+console.log(indexedUsers.findByField("name", "Dasha"))
+console.log(indexedUsers.findByField("id", "142"))
+console.log(indexedUsers.findByField("name", "Dmitry"))
+console.log(indexedUsers.findByField("age", "32"))
